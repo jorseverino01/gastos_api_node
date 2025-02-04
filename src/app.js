33d76c29bcd97c51c755faf40e45dea506824554
jlseverino <jorseverino01@gmail.com>
2025-02-04 18:45:38 -0500

    ajuste para que el api se ajuste acepte nueva url

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const app = express();
-const path = require('path');
-const morgan = require('morgan');
-const cors = require('cors');
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
 
 //Setings
-app.set('port', 5000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("port", 5000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 const corsOptions = {
-    origin: 'http://127.0.0.1:5500',
-    optionsSuccessStatus: 200
-}
+  origin: "https://www.desarrolladorweb.site/appGastos/",
+  optionsSuccessStatus: 200,
+};
 
 //Middelwares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
 app.use(express.json());
 
 //Routes
-app.use(require('./routes/index'));
+app.use(require("./routes/index"));
 
 //Statics
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //404 handler
 app.use((req, res, next) => {
-    res.status(404).send("404 not found")
-})
+  res.status(404).send("404 not found");
+});
 
 module.exports = app;
