792242fbc80c2baceceebaa292341cfb5838bd64
jlseverino <jorseverino01@gmail.com>
2025-02-06 23:11:15 -0500

    Funcion eliminado de registro agregado

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const corsOptions = {
     "https://desarrolladorweb.site",
     "https://www.desarrolladorweb.site/appGastos/",
     "https://desarrolladorweb.site/appGastos/",
+    "http://127.0.0.1:5500",
   ], // Agrega los dominios permitidos
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const clientDb = require("../mongodb");
 const { debug } = require("console");
+const { ObjectId } = require("mongodb");
 
 router.get("/", async (req, res) => {
   try {
@@ -40,7 +41,7 @@ router.post("/new_entry", async (req, res) => {
     !datos_fecha ||
     !fecha_compra
   ) {
-    res.status(400).send("Datos incompletos.");
+    res.status(400).send("DATA_INCOMPLETE");
     return;
   }
 
@@ -58,10 +59,41 @@ router.post("/new_entry", async (req, res) => {
       datos_fecha: datos_fecha,
       fecha_compra: fecha_compra,
     });
-    msg = "Gasto guardado correctamente.";
+    msg = "SUCCESS";
   } catch (err) {
     console.error("Error al conectar con MongoDB Atlas:", err);
-    msg = "Error al momento de guardar el gasto.";
+    msg = "ERROR";
+  } finally {
+    // Cerrar la conexión
+    // await client.close();
+  }
+  res.send(msg);
+});
+
+router.post("/delete_id", async (req, res) => {
+  const { id } = req.body;
+
+  let msg = "";
+
+  if (!id || !ObjectId.isValid(id)) {
+    return res.status(400).send({ msg: "INVALID_ID" });
+  }
+
+  try {
+    // Seleccionar base de datos y colección
+    const db = clientDb.db("gastospersonales");
+    const collection = db.collection("gasto");
+
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount > 0) {
+      msg = "SUCCESS";
+    } else {
+      msg = "NOT_FOUND"; // No se encontró el documento a eliminar
+    }
+  } catch (err) {
+    console.error("Error al conectar con MongoDB Atlas:", err);
+    msg = "ERROR";
   } finally {
     // Cerrar la conexión
     // await client.close();
